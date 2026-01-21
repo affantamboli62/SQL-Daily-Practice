376d8a22eaba4a643c49df7d8f150c63dfd424e5
Affan Tamboli <affantamboli62@gmail.com>
2026-01-21 22:09:24 +0530

    Add files via upload

---
+++ Day1.Basics SQl.sql
@@ -0,0 +1,18 @@
+#script of code
+#comandline==>terminal==>interactive mode
+#workbench===>
+#Basic commands
+show databases;
+#ctrl+enter
+#To select any database
+use sakila;
+show tables;
+#new database
+create database speedup;
+#CRUD operation==>create read update delete
+#Types of commands
+#1.DDL: create, alter,rename table,truncate ,drop
+#2.DML:
+#3.DCL
+#4.TCL
+#5.DQL: select
